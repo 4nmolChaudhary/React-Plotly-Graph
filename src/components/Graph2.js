94eb40b6e778a1b246a5db8541a5e71d9f909566
Anmol Chaudhary <achaudhary@netanalytics.co>
2020-10-21 09:45:01 +0530

    graph 1 done

--- src/components/Graph2.js
+++ src/components/Graph2.js
@@ -1,68 +1,56 @@
 import React, { useEffect, useState } from 'react'
 import Plot from 'react-plotly.js'
-import Plotly from 'plotly.js'
+import moment from 'moment'
+import axios from 'axios'
 
 function Graph2() {
-  const [data, setData] = useState()
+	const [data, setData] = useState()
 
-  useEffect(() => {
-    ;(async () => {
-      Plotly.d3.csv('https://raw.githubusercontent.com/4nmolChaudhary/Assets/master/utilization.csv', function (data) {
-        processData(data)
-      })
+	useEffect(() => {
+		;(async () => {
+			const response = await axios.get(`http://192.168.1.51:9090/api/v1/query?query=ifSpeed%7BifDescr=%22FastEthernet0/0%22,instance=%2210.1.1.1%22,job=%22snmp%22%7D[60d]`)
+			const data = response.data.data.result[0].values
+			const dates = []
+			const values = []
+			for (let i = 0; i < data.length; i++) {
+				dates.push(moment.unix(data[i][0]).format('YYYY-MM-DD hh:mm:ss'))
+				values.push(data[i][1] / 1000000)
+			}
+			//console.log(dates)
 
-      function processData(Data) {
-        // const date = Data.map(instance => {
-        //   //console.log(instance.date.split(' ')[0])
-        //   const x = new Date(instance.date)
-        //   return x.toISOString().split('.')[0].split('T').join(' ')
-        // })
-        // const date = ['2013-10-04 22:23:00', '2013-10-04 22:24:00', '2013-10-04 22:25:00']
-        // const mem = [1, 2, 3] //Data.map(instance => instance.mem)
-        // const cpu = [1, 5, 7] //Data.map(instance => instance.cpu)
-        // const storage = [4, 9, 5] //Data.map(instance => instance.storage)
-        // console.log(date)
-        // const trace1 = { type: 'scatter', mode: 'lines', name: 'Memory', x: date, y: mem, line: { color: '#17BECF' } }
-        // const trace2 = { type: 'scatter', mode: 'lines', name: 'CPU', x: date, y: cpu, line: { color: '#7F7F7F' } }
-        // const trace3 = { type: 'scatter', mode: 'lines', name: 'Storage', x: date, y: storage, line: { color: '#00CCFF' } }
-        // setData([trace1, trace2, trace3])
-        setData([
-          {
-            x: ['1462060800000', '1462147200000', '1462233600000'],
-            y: [1, 3, 6],
-            type: 'scatter',
-          },
-        ])
-      }
-    })()
-  }, [])
+			const trace1 = { type: 'scatter', mode: 'lines', name: 'ifSpeed', x: dates, y: values, line: { color: '#17BECF' } }
+			// const trace2 = { type: 'scatter', mode: 'lines', name: 'CPU', x: date, y: cpu, line: { color: '#7F7F7F' } }
+			// const trace3 = { type: 'scatter', mode: 'lines', name: 'Storage', x: date, y: storage, line: { color: '#00CCFF' } }
+			setData([trace1])
+		})()
+	}, [])
 
-  const Layout = {
-    title: {
-      text: 'Device Utilization',
-      font: {
-        family: 'GoogleSans-Bold',
-        weight: 800,
-        size: 22,
-      },
-      x: 0.0,
-    },
-    legend: { font: { family: 'GoogleSans-Regular', size: 12 } },
-    xaxis: { autorange: true, type: 'linear', tickfont: { family: 'GoogleSans-Regular' } },
-    yaxis: { autorange: true, type: 'linear', ticksuffix: ' %', tickfont: { family: 'GoogleSans-Regular' } },
-    width: 1200,
-    height: 400,
-    margin: { l: 60, r: 50, b: 0, t: 100, pad: 4 },
-  }
+	const Layout = {
+		title: {
+			text: 'ifSpeed',
+			font: {
+				family: 'GoogleSans-Bold',
+				weight: 800,
+				size: 22,
+			},
+			x: 0.0,
+		},
+		legend: { font: { family: 'GoogleSans-Regular', size: 12 } },
+		xaxis: { autorange: true, type: 'dates', tickfont: { family: 'GoogleSans-Regular' } },
+		yaxis: { autorange: true, type: 'linear', ticksuffix: ' Mbps', tickfont: { family: 'GoogleSans-Regular' } },
+		width: 1000,
+		height: 300,
+		margin: { l: 60, r: 50, b: 100, t: 50, pad: 4 },
+	}
 
-  const config = {
-    displaylogo: false,
-  }
-  return (
-    <div>
-      <Plot data={data} layout={Layout} config={config} />
-    </div>
-  )
+	const config = {
+		displaylogo: false,
+	}
+	return (
+		<div>
+			<Plot data={data} layout={Layout} config={config} />
+		</div>
+	)
 }
 
 export default Graph2
---
+++ src/components/Graph3.js
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from 'react'
+import Plot from 'react-plotly.js'
+import moment from 'moment'
+import axios from 'axios'
+
+function Graph3() {
+	const [data, setData] = useState()
+	useEffect(() => {
+		;(async () => {
+			const response = await axios.get(`http://192.168.1.51:9090/api/v1/query?query=ifHCInOctets%7BifDescr=%22FastEthernet0/0%22,instance=%2210.1.1.1%22,job=%22snmp%22%7D[60d]`)
+			const data = response.data.data.result[0].values
+			const dates = []
+			const values = []
+			for (let i = 0; i < data.length; i++) {
+				dates.push(moment.unix(data[i][0]).format('YYYY-MM-DD hh:mm:ss'))
+				values.push(data[i][1] / 1000000)
+			}
+			console.log(dates, values)
+
+			const trace1 = { type: 'scatter', mode: 'lines', name: 'ifSpeed', x: dates, y: values, line: { color: '#7F7F7F' } }
+			// const trace2 = { type: 'scatter', mode: 'lines', name: 'CPU', x: date, y: cpu, line: { color: '#7F7F7F' } }
+			// const trace3 = { type: 'scatter', mode: 'lines', name: 'Storage', x: date, y: storage, line: { color: '#00CCFF' } }
+			setData([trace1])
+		})()
+	}, [])
+
+	const Layout = {
+		title: {
+			text: 'ifSpeed',
+			font: {
+				family: 'GoogleSans-Bold',
+				weight: 800,
+				size: 22,
+			},
+			x: 0.0,
+		},
+		legend: { font: { family: 'GoogleSans-Regular', size: 12 } },
+		xaxis: { autorange: true, type: 'dates', tickfont: { family: 'GoogleSans-Regular' } },
+		yaxis: { autorange: true, type: 'linear', ticksuffix: ' Mbps', tickfont: { family: 'GoogleSans-Regular' } },
+		width: 1000,
+		height: 300,
+		margin: { l: 60, r: 50, b: 100, t: 50, pad: 4 },
+	}
+
+	const config = {
+		displaylogo: false,
+	}
+	return (
+		<div>
+			<Plot data={data} layout={Layout} config={config} />
+		</div>
+	)
+}
+
+export default Graph3
