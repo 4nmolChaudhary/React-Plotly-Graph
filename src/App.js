f01c08cfb4d7ccbe532c37f2069f834c3772da48
Anmol Chaudhary <achaudhary@netanalytics.co>
2020-10-21 14:35:11 +0530

    xx

--- src/App.js
+++ src/App.js
@@ -1,32 +1,15 @@
 import React from 'react'
 import './App.css'
-// import axios from 'axios'
-// import Graph1 from './components/Graph1'
-// import moment from 'moment'
+//import Graph1 from './components/Graph1'
 import Graph2 from './components/Graph2'
-import Graph3 from './components/Graph3'
+// import Graph3 from './components/Graph3'
 
 function App() {
-	// useEffect(() => {
-	// 	;(async () => {
-	// 		const response = await axios.get(`http://192.168.1.51:9090/api/v1/query?query=ifSpeed%7BifDescr=%22FastEthernet0/0%22,instance=%2210.1.1.1%22,job=%22snmp%22%7D[60d]`)
-	// 		const data = response.data.data.result[0].values
-	// 		console.log(data[0][0])
-	// 		const dates = []
-	// 		const values = []
-	// 		for (let i = 0; i < data.length; i++) {
-	// 			dates.push(`${moment.unix(data[i][0]).format().split('T')[0]} ${moment.unix(data[i][0]).format().split('T')[1].split('+')[0]}`)
-	// 			values.push(data[i][1])
-	// 		}
-	// 		console.log(dates, values)
-	// 	})()
-	// }, [])
-
 	return (
 		<div className="App">
 			{/* <Graph1 /> */}
 			<Graph2 />
-			<Graph3 />
+			{/* <Graph3 /> */}
 		</div>
 	)
 }
--- src/components/Graph2.js
+++ src/components/Graph2.js
@@ -8,18 +8,19 @@ function Graph2() {
 
 	useEffect(() => {
 		;(async () => {
-			const response = await axios.get(`http://192.168.1.51:9090/api/v1/query?query=ifSpeed%7BifDescr=%22FastEthernet0/0%22,instance=%2210.1.1.1%22,job=%22snmp%22%7D[60d]`)
+			const response = await axios.get(`http://192.168.1.51:9090/api/v1/query?query=ifInOctets{ifDescr=%22FastEthernet0/0%22,instance=%2210.1.1.1%22,job=%22snmp%22}[30d]`)
 			const data = response.data.data.result[0].values
 			const dates = []
 			const values = []
-			for (let i = 0; i < data.length; i++) {
-				dates.push(moment.unix(data[i][0]).format('YYYY-MM-DD hh:mm:ss'))
-				values.push(data[i][1] / 1000000)
-			}
-			//console.log(dates)
 
-			const trace1 = { type: 'scatter', mode: 'lines', name: 'ifSpeed', x: dates, y: values, line: { color: '#17BECF' } }
-			// const trace2 = { type: 'scatter', mode: 'lines', name: 'CPU', x: date, y: cpu, line: { color: '#7F7F7F' } }
+			for (let i = 0; i < data.length; i++) {
+				dates.push(moment.unix(data[i][0]).format('YYYY-MM-DD HH:mm:ss'))
+				//values.push(data[i][1] / 1000000)
+				values.push(Math.random() * 100)
+			}
+			console.log(dates)
+			const trace1 = { type: 'scatter', mode: 'lines', fill: 'toself', hoveron: 'points+fills', name: 'if_in_octets', x: dates, y: values, fillcolor: '#b0b6fd', line: { color: '#636efa' }, showLegend: true }
+			//const trace2 = { type: 'scatter', mode: 'lines', name: 'CPU', x: dates, y: dvals, line: { color: '#7F7F7F' } }
 			// const trace3 = { type: 'scatter', mode: 'lines', name: 'Storage', x: date, y: storage, line: { color: '#00CCFF' } }
 			setData([trace1])
 		})()
@@ -27,7 +28,7 @@ function Graph2() {
 
 	const Layout = {
 		title: {
-			text: 'ifSpeed',
+			text: 'Device Bandwidth',
 			font: {
 				family: 'GoogleSans-Bold',
 				weight: 800,
@@ -35,12 +36,13 @@ function Graph2() {
 			},
 			x: 0.0,
 		},
-		legend: { font: { family: 'GoogleSans-Regular', size: 12 } },
+
+		legend: { font: { family: 'GoogleSans-Regular', size: 12 }, legend: { orientation: 'h' } },
 		xaxis: { autorange: true, type: 'dates', tickfont: { family: 'GoogleSans-Regular' } },
-		yaxis: { autorange: true, type: 'linear', ticksuffix: ' Mbps', tickfont: { family: 'GoogleSans-Regular' } },
+		yaxis: { autorange: true, range: [1, 100], type: 'linear', ticksuffix: ' Mbps', tickfont: { family: 'GoogleSans-Regular' } },
 		width: 1000,
 		height: 300,
-		margin: { l: 60, r: 50, b: 100, t: 50, pad: 4 },
+		margin: { l: 60, r: 50, b: 50, t: 80, pad: 4 },
 	}
 
 	const config = {
